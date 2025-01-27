0ceaa8b63dada15d01f454487eca432f75701ba5
matheusrebola <m.cavalcantir@outlook.com>
2025-01-27 11:37:26 -0300

    feat: atualização dos microserviços

---
+++ services/reciever/src/main/java/soccervs/reciever/RecieverApplication.java
@@ -0,0 +1,13 @@
+package soccervs.reciever;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecieverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecieverApplication.class, args);
+	}
+
+}
