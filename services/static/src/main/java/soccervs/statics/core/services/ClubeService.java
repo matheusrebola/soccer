066113938dfe1021967c6295a72da6a9cb8097e2
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 12:22:20 -0300

    feat: criação do método de salvamento da entidade

--- services/static/src/main/java/soccervs/statics/core/services/ClubeService.java
+++ services/static/src/main/java/soccervs/statics/core/services/ClubeService.java
@@ -1,9 +1,18 @@
 package soccervs.statics.core.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import soccervs.statics.core.entities.Clubes;
+import soccervs.statics.core.repositories.ClubeRepository;
 
 @Service
 public class ClubeService {
+	@Autowired
+	private ClubeRepository repository;
 	
+	public Clubes save(Clubes clube) {
+		return repository.save(clube);
+	}
 
 }
