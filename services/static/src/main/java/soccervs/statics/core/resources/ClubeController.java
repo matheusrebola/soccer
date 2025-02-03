4d2a98cae46d947075f6c3f4a2e15eaf0207bb2d
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 13:01:18 -0300

    feat: configuração do recurso para salvamento de clubes com retorno de resposta http e uri com id

--- services/static/src/main/java/soccervs/statics/core/resources/ClubeController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ClubeController.java
@@ -1,16 +1,37 @@
 package soccervs.statics.core.resources;
 
+import java.net.URI;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import soccervs.statics.core.dtos.ClubeCreateDTO;
+import soccervs.statics.core.dtos.ClubeDTO;
+import soccervs.statics.core.entities.Clubes;
+import soccervs.statics.core.mappers.ClubeMapper;
+import soccervs.statics.core.services.ClubeService;
 
 @RestController
 @RequestMapping("/clubes")
 public class ClubeController {
 	
+	@Autowired
+    private ClubeMapper mapper;
+	
+	@Autowired
+	private ClubeService service;
+	
 	@PostMapping
-	public ResponseEntity<T> cadastrarClube(){
-		return null;
+	public ResponseEntity<ClubeDTO> cadastrarClube(@RequestBody ClubeCreateDTO createDTO){
+		Clubes clube = mapper.map(createDTO);
+		Clubes clubeSaved = service.save(clube);
+		Short id = service.getId(clubeSaved);
+		ClubeDTO dto = mapper.map(clubeSaved);
+		URI location = URI.create("/clubes/" + id);
+		return ResponseEntity.created(location).body(dto);
 	}
 }
