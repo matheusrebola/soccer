1956e7e96a56ef1c1553fb7f6d0cc53eb01f365b
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 19:03:39 -0300

    feat: ajuste na identação do código

--- services/static/src/main/java/soccervs/statics/core/resources/exceptions/NotFoundedException.java
+++ services/static/src/main/java/soccervs/statics/core/resources/exceptions/NotFoundedException.java
@@ -1,7 +1,7 @@
 package soccervs.statics.core.resources.exceptions;
 
 public class NotFoundedException extends RuntimeException {
-private static final long serialVersionUID = 1L;
+	private static final long serialVersionUID = 1L;
 	
 	public NotFoundedException(String msg) {
 		super(msg);
