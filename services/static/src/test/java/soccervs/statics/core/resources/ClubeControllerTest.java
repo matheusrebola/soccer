ec1e91683a226327e7e72ca8b26304dd6f966be1
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 11:45:59 -0300

    feat: criação dos métodos que irão conter as funcionalidades do endpoint

---
+++ services/static/src/test/java/soccervs/statics/core/resources/ClubeControllerTest.java
@@ -0,0 +1,41 @@
+package soccervs.statics.core.resources;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ClubeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+	
+	@Test
+	void testcadastrarClube() {
+		
+	}
+	
+	@Test
+	void testbuscarClubePorNome() {
+		
+	}
+	
+	@Test
+	void testBuscarPorCidade() {
+		
+	}
+	
+	@Test
+	void testBucarPorPais() {
+		
+	}
+	
+	@Test
+	void testBuscarPorTecnico() {
+		
+	}
+
+}
