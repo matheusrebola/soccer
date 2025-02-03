e94d12edf4b6cedf2572f2daf9f657e85fc7e0aa
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 12:07:30 -0300

    feat: configuração do DTO com seus atributos e anotação Data

--- services/static/src/main/java/soccervs/statics/core/dtos/ClubeCreateDTO.java
+++ services/static/src/main/java/soccervs/statics/core/dtos/ClubeCreateDTO.java
@@ -1,6 +1,12 @@
 package soccervs.statics.core.dtos;
 
-public record ClubeCreateDTO(
-	) {
+import lombok.Data;
 
+@Data
+public class ClubeCreateDTO {
+	private String nome;
+	private String cidade;
+	private String pais;
+	private String dataFundacao;
+	private Integer tecnico;
 }
