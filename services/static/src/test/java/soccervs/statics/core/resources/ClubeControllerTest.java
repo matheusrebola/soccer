1c69bdf79401e951fa9ab93d880e75bd736001e5
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 12:01:12 -0300

    feat: criação dos tópicos de sucesso e de falha

--- services/static/src/test/java/soccervs/statics/core/resources/ClubeControllerTest.java
+++ services/static/src/test/java/soccervs/statics/core/resources/ClubeControllerTest.java
@@ -1,25 +1,41 @@
 package soccervs.statics.core.resources;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import soccervs.statics.core.dtos.ClubeCreateDTO;
 
 @SpringBootTest
+@AutoConfigureMockMvc
 class ClubeControllerTest {
-
+	
+	@Autowired
+    private MockMvc mockMvc;
+	
 	@Test
-	void test() {
-		fail("Not yet implemented");
+	void testcadastrarClube_sucess(ClubeCreateDTO createDTO) throws Exception {
+		mockMvc.perform(post("/clubes"))
+        .andExpect(status().isCreated());
 	}
 	
 	@Test
-	void testcadastrarClube() {
+	void testcadastrarClube_fail() throws Exception {
 		
 	}
 	
 	@Test
-	void testbuscarClubePorNome() {
+	void testbuscarClubePorNome_sucess() {
+		
+	}
+	
+	@Test
+	void testbuscarClubePorNome_notfound() {
 		
 	}
 	
