84c67a32976fd51f7f048bba0e647eef5eb6452d
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 11:54:48 -0300

    feat: criação do método para cadastro

--- services/static/src/main/java/soccervs/statics/core/resources/ClubeController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ClubeController.java
@@ -1,10 +1,14 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/clubes")
 public class ClubeController {
-
+	
+	public ResponseEntity<T> cadastrarClube(){
+		return null;
+	}
 }
