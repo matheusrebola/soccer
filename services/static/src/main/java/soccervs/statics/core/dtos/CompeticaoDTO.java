fdbdc09923056376dcafa444b8dd28ae0c02c021
matheusrebola <m.cavalcantir@outlook.com>
2025-02-03 20:47:15 -0300

    feat: criação do DTO que será passado como resposta da persistencia

---
+++ services/static/src/main/java/soccervs/statics/core/dtos/CompeticaoDTO.java
@@ -0,0 +1,21 @@
+package soccervs.statics.core.dtos;
+
+import java.time.Year;
+
+import lombok.Data;
+import soccervs.statics.core.entities.enums.ECompeticao;
+import soccervs.statics.core.entities.enums.EDisputa;
+
+@Data
+public class CompeticaoDTO {
+	private Short id;
+	private String nome;
+	private Year ano;
+	private ECompeticao tipoCompeticao;
+	private EDisputa tipoDisputa;
+	private String paisRegiao;
+	private Integer titulo;
+	private Short temporada;
+	private Long resultado;
+	private Long partida;
+}
