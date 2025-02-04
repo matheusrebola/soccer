0604d0487eb5810bdb106b653ae2489da72ec21e
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 15:29:37 -0300

    feat: configuração da classe que será o endpoint de jogador

---
+++ services/static/src/main/java/soccervs/statics/core/resources/JogadoresController.java
@@ -0,0 +1,10 @@
+package soccervs.statics.core.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/jogadores")
+public class JogadoresController {
+
+}
