98c21dca64dc8600119975f4b1f06697daee1299
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:25:41 -0300

    feat: criação da classe para o endpoint de elencos + criação dos métodos de save e findall

---
+++ services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
@@ -0,0 +1,17 @@
+package soccervs.statics.core.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/elencos")
+public class ElencosController {
+
+	public void cadastrarElencos() {
+		
+	}
+	
+	public void encontrarTodos() {
+		
+	}
+}
