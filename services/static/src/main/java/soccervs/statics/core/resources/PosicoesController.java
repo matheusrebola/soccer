4dcb469fad274614cea6aa97247f749098656c2a
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:13:32 -0300

    feat: criação da classe responsável por fazer o mapeamento entidade-dto e vice-versa

--- services/static/src/main/java/soccervs/statics/core/resources/PosicoesController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/PosicoesController.java
@@ -1,11 +1,16 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import soccervs.statics.core.mappers.PosicoesMapper;
 
 @RestController
 @RequestMapping("/partidas")
 public class PosicoesController {
 
+	@Autowired
+	private PosicoesMapper mapper;
 	
 }
