7be00b36dda4902997d5d5e796c3b737b8d3635f
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:30:29 -0300

    feat: configuração do DTO com os atriutos

--- services/static/src/main/java/soccervs/statics/core/dtos/ElencoCreateDTO.java
+++ services/static/src/main/java/soccervs/statics/core/dtos/ElencoCreateDTO.java
@@ -1,8 +1,16 @@
 package soccervs.statics.core.dtos;
 
+import java.util.Set;
+
 import lombok.Data;
 
 @Data
 public class ElencoCreateDTO {
-	
+	private String nome;
+	private Short anoFormacao;
+	private String tecnicoResponsavel;
+	private Integer tecnico;
+	private Short clube;
+	private Short temporada;
+	private Set<Integer> jogadoresElenco;
 }
