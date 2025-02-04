1a5a779f9b41bc4846de5e978ec810e315bd5af1
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:27:45 -0300

    feat: criação do DTO que será recebido para a criação dos elencos

--- services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
@@ -1,16 +1,23 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import soccervs.statics.core.dtos.ElencoCreateDTO;
 
 @RestController
 @RequestMapping("/elencos")
 public class ElencosController {
 
-	public void cadastrarElencos() {
+	@PostMapping
+	public void cadastrarElencos(@RequestBody ElencoCreateDTO create) {
 		
 	}
 	
+	@GetMapping
 	public void encontrarTodos() {
 		
 	}
