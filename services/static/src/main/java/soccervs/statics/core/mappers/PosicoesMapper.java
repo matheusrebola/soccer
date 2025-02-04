2b647067abeec990c2b5fc8b8c6a5451b9114e92
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:15:52 -0300

    feat: configuração das variáveis das classes

--- services/static/src/main/java/soccervs/statics/core/mappers/PosicoesMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/PosicoesMapper.java
@@ -1,5 +1,12 @@
 package soccervs.statics.core.mappers;
 
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class PosicoesMapper {
 
+	@Autowired
+	private ModelMapper mapper;
 }
--- services/static/src/main/java/soccervs/statics/core/services/PosicoesService.java
+++ services/static/src/main/java/soccervs/statics/core/services/PosicoesService.java
@@ -1,8 +1,13 @@
 package soccervs.statics.core.services;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import soccervs.statics.core.repositories.PosicaoRepository;
 
 @Service
 public class PosicoesService {
 
+	@Autowired
+	private PosicaoRepository repository;
 }
