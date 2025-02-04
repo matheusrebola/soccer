03fbcf0a43017ba27e33c2d3370774d690b49d4c
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:48:46 -0300

    feat: configuração final do método de cadastro de elencos

--- services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
@@ -1,6 +1,9 @@
 package soccervs.statics.core.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,8 +11,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import soccervs.statics.core.dtos.ElencoCreateDTO;
+import soccervs.statics.core.dtos.ElencoDTO;
 import soccervs.statics.core.entities.Elencos;
 import soccervs.statics.core.mappers.ElencoMapper;
+import soccervs.statics.core.resources.exceptions.NotPersistedException;
+import soccervs.statics.core.services.ElencoService;
 
 @RestController
 @RequestMapping("/elencos")
@@ -21,9 +27,17 @@ public class ElencosController {
 	private ElencoService service;
 
 	@PostMapping
-	public void cadastrarElencos(@RequestBody ElencoCreateDTO create) {
+	public ResponseEntity<ElencoDTO> cadastrarElencos(@RequestBody ElencoCreateDTO create) {
 		Elencos elenco = mapper.map(create);
 		Elencos saved = service.salvar(elenco);
+		
+		if (saved == null) {
+			throw new NotPersistedException("Competicao não persistida");
+		}
+		Integer id = service.pegarId(elenco);
+		URI location = URI.create("/elencos/"+id);
+		ElencoDTO dto = mapper.map(saved);
+		return ResponseEntity.created(location).body(dto);
 	}
 	
 	@GetMapping
