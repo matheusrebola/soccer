dc6fa4d7cebd6ea64b8bdeebb38967ac7dbabb02
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:55:28 -0300

    feat: criação do método que irá mapear dto em entidade

--- services/static/src/main/java/soccervs/statics/core/mappers/ResultadosMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/ResultadosMapper.java
@@ -4,9 +4,17 @@ import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import soccervs.statics.core.dtos.ResultadoCreateDTO;
+import soccervs.statics.core.entities.Resultados;
+
 @Component
 public class ResultadosMapper {
 
 	@Autowired
 	private ModelMapper mapper;
+
+	public Resultados map(ResultadoCreateDTO createDTO) {
+		Resultados r = mapper.map(createDTO, Resultados.class);
+		return r;
+	}
 }
--- services/static/src/main/java/soccervs/statics/core/resources/ResultadosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ResultadosController.java
@@ -5,6 +5,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import soccervs.statics.core.dtos.ResultadoCreateDTO;
+import soccervs.statics.core.entities.Resultados;
 import soccervs.statics.core.mappers.ResultadosMapper;
 import soccervs.statics.core.services.ResultadosService;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +23,7 @@ public class ResultadosController {
 	
 	@PostMapping
 	public void cadastrarResultado(@RequestBody ResultadoCreateDTO createDTO) {
-		
+		Resultados resultado = mapper.map(createDTO);
 	}
 	
 }
