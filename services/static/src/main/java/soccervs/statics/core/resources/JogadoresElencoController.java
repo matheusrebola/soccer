0270c3a452a1a07edcd0ef095b23d4aabf6212ae
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 16:08:11 -0300

    feat: criação das variáveis e métodos para criação de entidades e busca

--- services/static/src/main/java/soccervs/statics/core/resources/JogadoresElencoController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/JogadoresElencoController.java
@@ -1,10 +1,36 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import soccervs.statics.core.mappers.JElencoMapper;
+import soccervs.statics.core.services.JElencoService;
+
+
 
 @RestController
 @RequestMapping("/jogadores-elenco")
 public class JogadoresElencoController {
-
+	
+	@Autowired
+	private JElencoMapper mapper;
+	
+	@Autowired
+	private JElencoService service;
+	
+	@PostMapping
+	public void postMethodName(@RequestBody String entity) {
+		
+	}
+	
+	
+	@GetMapping
+	public void encontrarTodos() {
+		
+	}
+	
 }
