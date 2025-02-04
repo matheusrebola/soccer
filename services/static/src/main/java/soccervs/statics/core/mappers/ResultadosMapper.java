da26a89f18dda4b6fa7189e512968aba87051a66
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 19:05:34 -0300

    feat: configuração do método que mapeia entidade em dto

--- services/static/src/main/java/soccervs/statics/core/mappers/ResultadosMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/ResultadosMapper.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import soccervs.statics.core.dtos.ResultadoCreateDTO;
+import soccervs.statics.core.dtos.ResultadoDTO;
 import soccervs.statics.core.entities.Resultados;
 
 @Component
@@ -16,5 +17,10 @@ public class ResultadosMapper {
 	public Resultados map(ResultadoCreateDTO createDTO) {
 		Resultados r = mapper.map(createDTO, Resultados.class);
 		return r;
+	}
+
+	public ResultadoDTO map(Resultados saved) {
+		ResultadoDTO dto = mapper.map(saved, ResultadoDTO.class);
+		return dto;
 	}
 }
