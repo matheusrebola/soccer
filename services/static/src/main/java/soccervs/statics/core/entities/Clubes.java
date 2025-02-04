0d2a080f6c46eda5af954f5f7ab9bd9be81f1f60
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 11:03:13 -0300

    feat: correção do tipo do atributo e anotação

--- services/static/src/main/java/soccervs/statics/core/entities/Clubes.java
+++ services/static/src/main/java/soccervs/statics/core/entities/Clubes.java
@@ -1,5 +1,7 @@
 package soccervs.statics.core.entities;
 
+import java.time.LocalDate;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
@@ -23,8 +25,8 @@ public class Clubes {
 	@Column(length = 50)
 	private String pais;
 	
-	@Column(length = 10)
-	private String dataFundacao;
+	@Column(name = "data_fundacao")
+	private LocalDate dataFundacao;
 	
 	@ManyToOne @JoinColumn(name = "tecnico_id", nullable = false)
 	private Tecnicos tecnico;
