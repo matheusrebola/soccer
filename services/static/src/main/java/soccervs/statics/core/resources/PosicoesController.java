b463946137efe679cc3982d74b04fd5bd026af86
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:12:10 -0300

    feat: criação da classe de controle dos endpoints

---
+++ services/static/src/main/java/soccervs/statics/core/resources/PosicoesController.java
@@ -0,0 +1,11 @@
+package soccervs.statics.core.resources;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/partidas")
+public class PosicoesController {
+
+	
+}
