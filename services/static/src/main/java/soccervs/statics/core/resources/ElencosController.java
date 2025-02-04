8382c38d29cba88f63f6263d088572bdb369569e
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:37:14 -0300

    feat: configuração do salvamento da entidade

--- services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ElencosController.java
@@ -1,5 +1,6 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -7,14 +8,22 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import soccervs.statics.core.dtos.ElencoCreateDTO;
+import soccervs.statics.core.entities.Elencos;
+import soccervs.statics.core.mappers.ElencoMapper;
 
 @RestController
 @RequestMapping("/elencos")
 public class ElencosController {
+	
+	@Autowired
+	private ElencoMapper mapper; 
+	
+	private ElencoService service;
 
 	@PostMapping
 	public void cadastrarElencos(@RequestBody ElencoCreateDTO create) {
-		
+		Elencos elenco = mapper.map(create);
+		Elencos saved = service.salvar(elenco);
 	}
 	
 	@GetMapping
