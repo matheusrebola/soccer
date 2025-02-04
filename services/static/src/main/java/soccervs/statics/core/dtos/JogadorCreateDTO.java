f09ecac7147e3385e02761c1a7e86658c833a896
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 15:36:32 -0300

    feat: criação do DTO que será recebido no corpo da requisição para a criação da entidade

---
+++ services/static/src/main/java/soccervs/statics/core/dtos/JogadorCreateDTO.java
@@ -0,0 +1,19 @@
+package soccervs.statics.core.dtos;
+
+import java.time.LocalDate;
+import java.util.Set;
+
+import lombok.Data;
+import soccervs.statics.core.entities.enums.EPePreferido;
+
+@Data
+public class JogadorCreateDTO {
+	private String nome;
+	private LocalDate dataNascimento;
+	private String nacionalidade;
+	private EPePreferido pePreferido;
+	private Float altura;
+	private Float peso;
+	private Set<Byte> posicao;
+	private Set<Integer> jogadoresElenco;
+}
