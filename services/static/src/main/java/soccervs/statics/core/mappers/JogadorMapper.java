0379d2cc991ba2fd1f7541ea047dae760c8f73d9
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 15:37:57 -0300

    feat: criação do método para transformar DTO em entidade

---
+++ services/static/src/main/java/soccervs/statics/core/mappers/JogadorMapper.java
@@ -0,0 +1,22 @@
+package soccervs.statics.core.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import soccervs.statics.core.dtos.JogadorCreateDTO;
+import soccervs.statics.core.entities.Jogadores;
+
+@Component
+public class JogadorMapper {
+
+	@Autowired
+	private ModelMapper mapper;
+
+	public Jogadores map(JogadorCreateDTO dto) {
+		Jogadores j = mapper.map(dto, Jogadores.class);
+		return j;
+	}
+	
+	
+}
