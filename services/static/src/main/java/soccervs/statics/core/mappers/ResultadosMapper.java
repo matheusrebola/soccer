01e204450d522f4c32f6967607ebb685897077ef
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:49:20 -0300

    feat: criação da classe responsável por mapear entidade-dto e vice-versa

---
+++ services/static/src/main/java/soccervs/statics/core/mappers/ResultadosMapper.java
@@ -0,0 +1,12 @@
+package soccervs.statics.core.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class ResultadosMapper {
+
+	@Autowired
+	private ModelMapper mapper;
+}
--- services/static/src/main/java/soccervs/statics/core/resources/ResultadosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/ResultadosController.java
@@ -1,5 +1,6 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +8,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/resultados")
 public class ResultadosController {
 
+	@Autowired
+	private ResultadosMapper mapper;
 }
