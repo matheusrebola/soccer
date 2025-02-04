deef6651d2fea7af278bd60cfeb20ab27d793114
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 18:17:04 -0300

    feat: criação do metodo para cadastrar posicoes

--- services/static/src/main/java/soccervs/statics/core/resources/PosicoesController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/PosicoesController.java
@@ -1,10 +1,13 @@
 package soccervs.statics.core.resources;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import soccervs.statics.core.mappers.PosicoesMapper;
+import soccervs.statics.core.services.PosicoesService;
 
 @RestController
 @RequestMapping("/partidas")
@@ -15,5 +18,9 @@ public class PosicoesController {
 	
 	@Autowired
 	private PosicoesService service;
+	
+	@PostMapping
+	public void cadastrarPosicao() {
+	}
 	
 }
