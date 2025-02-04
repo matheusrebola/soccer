cf6457fb463b9dd9021883d58f122cf8a4663cb6
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 14:34:14 -0300

    feat: configuração do método para mapeamento de dto para entidade elenco

--- services/static/src/main/java/soccervs/statics/core/mappers/ElencoMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/ElencoMapper.java
@@ -1,5 +1,19 @@
 package soccervs.statics.core.mappers;
 
-public class ElencoMapper {
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
 
+import soccervs.statics.core.dtos.ElencoCreateDTO;
+import soccervs.statics.core.entities.Elencos;
+
+@Component
+public class ElencoMapper {
+	@Autowired
+	private ModelMapper mapper;
+	
+	public Elencos map(ElencoCreateDTO dto) {
+		Elencos e = mapper.map(dto, Elencos.class);
+		return e;
+	}
 }
