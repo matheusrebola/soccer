0c9ae4915f0440fb3ae33d293a366c272fef0c1e
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 11:54:43 -0300

    feat: criação do método para buscar o ID

--- services/static/src/main/java/soccervs/statics/core/repositories/ClubeRepository.java
+++ services/static/src/main/java/soccervs/statics/core/repositories/ClubeRepository.java
@@ -3,6 +3,8 @@ package soccervs.statics.core.repositories;
 import java.util.Set;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import soccervs.statics.core.dtos.ClubeByCityDTO;
@@ -10,7 +12,9 @@ import soccervs.statics.core.entities.Clubes;
 
 @Repository
 public interface ClubeRepository extends JpaRepository<Clubes, Short> {
-	Short getId(Clubes clube);
+	
+	@Query("SELECT c.id FROM Clubes c WHERE c = :clube")
+    Short findIdByClube(@Param("clube") Clubes clube);
 
 	Set<ClubeByCityDTO> findByCity(String cidade);
 }
--- services/static/src/main/java/soccervs/statics/core/repositories/CompeticaoRepository.java
+++ services/static/src/main/java/soccervs/statics/core/repositories/CompeticaoRepository.java
@@ -1,13 +1,15 @@
 package soccervs.statics.core.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import soccervs.statics.core.entities.Competicoes;
 
 @Repository
 public interface CompeticaoRepository extends JpaRepository<Competicoes, Short> {
-	
-	Short findId(Competicoes c);
+	@Query("SELECT c.id FROM Competicoes c WHERE c = :competicao")
+    Short findIdByCompeticao(@Param("competicao") Competicoes competicao);
 
 }
