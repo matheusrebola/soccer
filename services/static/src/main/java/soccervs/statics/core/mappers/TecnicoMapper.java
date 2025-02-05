753df60f1139d782b885a3e7dea4662b4c321719
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 21:10:28 -0300

    feat: criação da classe que irá mapear entidades em dtos e vice-versa

---
+++ services/static/src/main/java/soccervs/statics/core/mappers/TecnicoMapper.java
@@ -0,0 +1,13 @@
+package soccervs.statics.core.mappers;
+
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
+public class TecnicoMapper {
+
+	@Autowired
+	private ModelMapper mapper;
+	
+}
