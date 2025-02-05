3bba5f1604046003c520f0d1578665fc31dc456f
matheusrebola <m.cavalcantir@outlook.com>
2025-02-04 21:07:11 -0300

    feat: criação do método de cadastro de tecnicos

--- services/static/src/main/java/soccervs/statics/core/resources/TecnicosController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/TecnicosController.java
@@ -1,5 +1,8 @@
 package soccervs.statics.core.resources;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +10,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/tecnicos")
 public class TecnicosController {
 
+	@Autowired
+	private TecnicoMapper mapper;
+	
+	@Autowired
+	private TecnicoService service;
+	
+	@PostMapping
+	public void cadastrarTecnico() {
+		
+	}
+	
 }
