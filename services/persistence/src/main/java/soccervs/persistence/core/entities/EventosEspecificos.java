5cf449dc63e5ca34d28baf468c2029d55745db4d
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 15:17:18 -0300

    feat: criação da entidade com seu enum

--- services/persistence/src/main/java/soccervs/persistence/core/entities/EventosEspecificos.java
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/EventosEspecificos.java
@@ -1,5 +1,64 @@
 package soccervs.persistence.core.entities;
 
+import soccervs.persistence.core.entities.enums.EEvento;
+
 public class EventosEspecificos {
+	private Long id;
+	private EEvento evento;
+	private Byte minuto;
+	private Long partidaId;
+	private Long jogadorId;
+
+	public EventosEspecificos(Long id, EEvento evento, Byte minuto, Long partidaId, Long jogadorId) {
+		super();
+		this.id = id;
+		this.evento = evento;
+		this.minuto = minuto;
+		this.partidaId = partidaId;
+		this.jogadorId = jogadorId;
+	}
+
+	public EventosEspecificos() {
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public EEvento getEvento() {
+		return evento;
+	}
+
+	public void setEvento(EEvento evento) {
+		this.evento = evento;
+	}
+
+	public Byte getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(Byte minuto) {
+		this.minuto = minuto;
+	}
+
+	public Long getPartidaId() {
+		return partidaId;
+	}
+
+	public void setPartidaId(Long partidaId) {
+		this.partidaId = partidaId;
+	}
+
+	public Long getJogadorId() {
+		return jogadorId;
+	}
+
+	public void setJogadorId(Long jogadorId) {
+		this.jogadorId = jogadorId;
+	}
 
 }
---
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/enums/EEvento.java
@@ -0,0 +1,5 @@
+package soccervs.persistence.core.entities.enums;
+
+public enum EEvento {
+	LESAO, ENTRADA, SAIDA, PAUSA
+}
