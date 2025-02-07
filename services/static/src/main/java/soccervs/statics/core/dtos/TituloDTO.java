3f6ed83aa2a506d1d4fdb9b00817468aae6cf7eb
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 11:29:42 -0300

    feat: criação do dto de retorno da criação da entidade

---
+++ services/static/src/main/java/soccervs/statics/core/dtos/TituloDTO.java
@@ -0,0 +1,35 @@
+package soccervs.statics.core.dtos;
+
+import java.time.Year;
+
+import soccervs.statics.core.entities.enums.ECompeticao;
+import soccervs.statics.core.entities.enums.EParticipante;
+
+public class TituloDTO extends TituloCreateDTO {
+	private Long id;
+
+	public TituloDTO(String nome, EParticipante tipoParticipante, String temporada, String posicaoFinal, Year conquista,
+			ECompeticao tipoTitulo, Long participante, Long clube, Long competicao, Long id) {
+		super(nome, tipoParticipante, temporada, posicaoFinal, conquista, tipoTitulo, participante, clube, competicao);
+		this.id = id;
+	}
+
+	public TituloDTO() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public TituloDTO(String nome, EParticipante tipoParticipante, String temporada, String posicaoFinal, Year conquista,
+			ECompeticao tipoTitulo, Long participante, Long clube, Long competicao) {
+		super(nome, tipoParticipante, temporada, posicaoFinal, conquista, tipoTitulo, participante, clube, competicao);
+		// TODO Auto-generated constructor stub
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+}
