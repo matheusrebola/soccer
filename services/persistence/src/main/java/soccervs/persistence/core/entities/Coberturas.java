0ecc8d54ac2b3537c711fd951a00d9dd5a5f0cbc
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 16:06:11 -0300

    feat: configuração da entidade de cobertura

--- services/persistence/src/main/java/soccervs/persistence/core/entities/Coberturas.java
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/Coberturas.java
@@ -1,5 +1,72 @@
 package soccervs.persistence.core.entities;
 
 public class Coberturas {
+	private Long id;
+	private Byte tentativasT;
+	private Byte tBemSucedidas;
+	private Float efetividade;
+	private Long jogadorId;
+	private Long partidaId;
+
+	public Coberturas(Long id, Byte tentativasT, Byte tBemSucedidas, Float efetividade, Long jogadorId,
+			Long partidaId) {
+		super();
+		this.id = id;
+		this.tentativasT = tentativasT;
+		this.tBemSucedidas = tBemSucedidas;
+		this.efetividade = efetividade;
+		this.jogadorId = jogadorId;
+		this.partidaId = partidaId;
+	}
+
+	public Coberturas() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Byte getTentativasT() {
+		return tentativasT;
+	}
+
+	public void setTentativasT(Byte tentativasT) {
+		this.tentativasT = tentativasT;
+	}
+
+	public Byte gettBemSucedidas() {
+		return tBemSucedidas;
+	}
+
+	public void settBemSucedidas(Byte tBemSucedidas) {
+		this.tBemSucedidas = tBemSucedidas;
+	}
+
+	public Float getEfetividade() {
+		return efetividade;
+	}
+
+	public void setEfetividade(Float efetividade) {
+		this.efetividade = efetividade;
+	}
+
+	public Long getJogadorId() {
+		return jogadorId;
+	}
+
+	public void setJogadorId(Long jogadorId) {
+		this.jogadorId = jogadorId;
+	}
+
+	public Long getPartidaId() {
+		return partidaId;
+	}
+
+	public void setPartidaId(Long partidaId) {
+		this.partidaId = partidaId;
+	}
 
 }
