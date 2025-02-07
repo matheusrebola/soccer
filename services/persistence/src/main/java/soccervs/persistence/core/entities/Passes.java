625b982ff1832f9109cc90e9feb833ed8585117f
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 15:40:43 -0300

    feat: configuração da entidade e seus enums

--- services/persistence/src/main/java/soccervs/persistence/core/entities/Passes.java
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/Passes.java
@@ -1,5 +1,83 @@
 package soccervs.persistence.core.entities;
 
-public class Passes {
+import soccervs.persistence.core.entities.enums.EPasse;
 
+public class Passes {
+	private Long id;
+	private EPasse passe;
+	private Byte tTotatis;
+	private Byte tBemSuce;
+	private Float efetividade;
+	private Long jogadorId;
+	private Long partidaId;
+
+	public Passes(Long id, EPasse passe, Byte tTotatis, Byte tBemSuce, Float efetividade, Long jogadorId,
+			Long partidaId) {
+		super();
+		this.id = id;
+		this.passe = passe;
+		this.tTotatis = tTotatis;
+		this.tBemSuce = tBemSuce;
+		this.efetividade = efetividade;
+		this.jogadorId = jogadorId;
+		this.partidaId = partidaId;
+	}
+
+	public Passes() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public EPasse getPasse() {
+		return passe;
+	}
+
+	public void setPasse(EPasse passe) {
+		this.passe = passe;
+	}
+
+	public Byte gettTotatis() {
+		return tTotatis;
+	}
+
+	public void settTotatis(Byte tTotatis) {
+		this.tTotatis = tTotatis;
+	}
+
+	public Byte gettBemSuce() {
+		return tBemSuce;
+	}
+
+	public void settBemSuce(Byte tBemSuce) {
+		this.tBemSuce = tBemSuce;
+	}
+
+	public Float getEfetividade() {
+		return efetividade;
+	}
+
+	public void setEfetividade(Float efetividade) {
+		this.efetividade = efetividade;
+	}
+
+	public Long getJogadorId() {
+		return jogadorId;
+	}
+
+	public void setJogadorId(Long jogadorId) {
+		this.jogadorId = jogadorId;
+	}
+
+	public Long getPartidaId() {
+		return partidaId;
+	}
+
+	public void setPartidaId(Long partidaId) {
+		this.partidaId = partidaId;
+	}
 }
