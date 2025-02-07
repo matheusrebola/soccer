1a843ad62a8dad8025142be4388543f04457c2fe
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 15:09:07 -0300

    feat: criação da entidade de cartões com seu enum

--- services/persistence/src/main/java/soccervs/persistence/core/entities/Cartoes.java
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/Cartoes.java
@@ -1,5 +1,72 @@
 package soccervs.persistence.core.entities;
 
-public class Cartoes {
+import soccervs.persistence.core.entities.enums.ECartao;
 
+public class Cartoes {
+	private Long id;
+	private Byte minuto;
+	private ECartao cartao;
+	private String motivo;
+	private Long jogadorId;
+	private Long partidaId;
+	
+	public Cartoes(Long id, Byte minuto, ECartao cartao, String motivo, Long jogadorId, Long partidaId) {
+		super();
+		this.id = id;
+		this.minuto = minuto;
+		this.cartao = cartao;
+		this.motivo = motivo;
+		this.jogadorId = jogadorId;
+		this.partidaId = partidaId;
+	}
+
+	public Cartoes() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public Byte getMinuto() {
+		return minuto;
+	}
+
+	public void setMinuto(Byte minuto) {
+		this.minuto = minuto;
+	}
+
+	public ECartao getCartao() {
+		return cartao;
+	}
+
+	public void setCartao(ECartao cartao) {
+		this.cartao = cartao;
+	}
+
+	public String getMotivo() {
+		return motivo;
+	}
+
+	public void setMotivo(String motivo) {
+		this.motivo = motivo;
+	}
+
+	public Long getJogadorId() {
+		return jogadorId;
+	}
+
+	public void setJogadorId(Long jogadorId) {
+		this.jogadorId = jogadorId;
+	}
+
+	public Long getPartidaId() {
+		return partidaId;
+	}
+
+	public void setPartidaId(Long partidaId) {
+		this.partidaId = partidaId;
+	}
 }
---
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/enums/ECartao.java
@@ -0,0 +1,5 @@
+package soccervs.persistence.core.entities.enums;
+
+public enum ECartao {
+	A,V
+}
