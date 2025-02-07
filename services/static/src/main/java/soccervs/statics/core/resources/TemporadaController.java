537aa326262a9f55037ebabdb38c7ad5ebf86088
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 10:31:24 -0300

    feat: necessidade de converter DTO em Entidade

--- services/static/src/main/java/soccervs/statics/core/resources/TemporadaController.java
+++ services/static/src/main/java/soccervs/statics/core/resources/TemporadaController.java
@@ -6,14 +6,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import soccervs.statics.core.dtos.TemporadaCreateDTO;
+import soccervs.statics.core.entities.Temporadas;
 
 @RestController
 @RequestMapping("/temporadas")
 public class TemporadaController {
 	
+	
+	
 	@PostMapping
 	public String cadastrarTemporada(@RequestBody TemporadaCreateDTO createDTO) {
-		
+		Temporadas temporada = mapper.map(createDTO);
 		
 		return entity;
 	}
