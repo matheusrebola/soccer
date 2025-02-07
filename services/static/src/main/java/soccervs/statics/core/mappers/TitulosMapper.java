5da5a18a389ee112937960a115a7171aa6f2b0be
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 11:29:59 -0300

    feat: criação do método para converter entidade em dto

--- services/static/src/main/java/soccervs/statics/core/mappers/TitulosMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/TitulosMapper.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import soccervs.statics.core.dtos.TituloCreateDTO;
+import soccervs.statics.core.dtos.TituloDTO;
 import soccervs.statics.core.entities.Titulos;
 
 @Component
@@ -17,6 +18,10 @@ public class TitulosMapper {
 
 	public Titulos map(TituloCreateDTO createDTO) {
 		return mapper.map(createDTO, Titulos.class);
+	}
+
+	public TituloDTO map(Titulos saved) {
+		return mapper.map(saved, TituloDTO.class);
 	}
 	
 
