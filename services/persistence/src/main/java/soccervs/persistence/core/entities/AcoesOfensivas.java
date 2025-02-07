d99421311e3fa4ce141e75c3f70ebe24bdabc63e
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 15:14:07 -0300

    feat: criação da entidade de acoes ofensivas + enum de acao

--- services/persistence/src/main/java/soccervs/persistence/core/entities/AcoesOfensivas.java
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/AcoesOfensivas.java
@@ -1,5 +1,83 @@
 package soccervs.persistence.core.entities;
 
-public class AcoesOfensivas {
+import soccervs.persistence.core.entities.enums.EAcao;
 
+public class AcoesOfensivas {
+	private Long id;
+	private EAcao acao;
+	private Byte tentativasT;
+	private Byte tBemSucedidas;
+	private Float efetividade;
+	private Long jogadorId;
+	private Long partidaId;
+	
+	public AcoesOfensivas(Long id, EAcao acao, Byte tentativasT, Byte tBemSucedidas, Float efetividade, Long jogadorId,
+			Long partidaId) {
+		super();
+		this.id = id;
+		this.acao = acao;
+		this.tentativasT = tentativasT;
+		this.tBemSucedidas = tBemSucedidas;
+		this.efetividade = efetividade;
+		this.jogadorId = jogadorId;
+		this.partidaId = partidaId;
+	}
+
+	public AcoesOfensivas() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public EAcao getAcao() {
+		return acao;
+	}
+
+	public void setAcao(EAcao acao) {
+		this.acao = acao;
+	}
+
+	public Byte getTentativasT() {
+		return tentativasT;
+	}
+
+	public void setTentativasT(Byte tentativasT) {
+		this.tentativasT = tentativasT;
+	}
+
+	public Byte gettBemSucedidas() {
+		return tBemSucedidas;
+	}
+
+	public void settBemSucedidas(Byte tBemSucedidas) {
+		this.tBemSucedidas = tBemSucedidas;
+	}
+
+	public Float getEfetividade() {
+		return efetividade;
+	}
+
+	public void setEfetividade(Float efetividade) {
+		this.efetividade = efetividade;
+	}
+
+	public Long getJogadorId() {
+		return jogadorId;
+	}
+
+	public void setJogadorId(Long jogadorId) {
+		this.jogadorId = jogadorId;
+	}
+
+	public Long getPartidaId() {
+		return partidaId;
+	}
+
+	public void setPartidaId(Long partidaId) {
+		this.partidaId = partidaId;
+	}
 }
---
+++ services/persistence/src/main/java/soccervs/persistence/core/entities/enums/EAcao.java
@@ -0,0 +1,5 @@
+package soccervs.persistence.core.entities.enums;
+
+public enum EAcao {
+	CRUZAMENTO, LANCAMENTO, FIN_CURTA, FIN_MEDIA, FIN_LONGA, PASSE_PROF
+}
