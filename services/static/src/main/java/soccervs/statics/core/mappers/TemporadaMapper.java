2eacb4817a202a791c7abd1dae37ab464ea3664e
matheusrebola <m.cavalcantir@outlook.com>
2025-02-07 11:08:54 -0300

    feat: criação do método para transformar lista de entidades em dtos

--- services/static/src/main/java/soccervs/statics/core/mappers/TemporadaMapper.java
+++ services/static/src/main/java/soccervs/statics/core/mappers/TemporadaMapper.java
@@ -1,5 +1,8 @@
 package soccervs.statics.core.mappers;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -22,6 +25,12 @@ public class TemporadaMapper {
 
 	public TemporadaDTO map(Temporadas saved) {
 		return mapper.map(saved, TemporadaDTO.class);
+	}
+
+	public List<TemporadaDTO> map(List<Temporadas> t) {
+		return t.stream()
+				.map(temporadas -> mapper.map(temporadas, TemporadaDTO.class))
+				.collect(Collectors.toList());
 	}
 	
 	
