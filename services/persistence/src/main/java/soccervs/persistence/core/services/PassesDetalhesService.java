d49141ee817e23518439c57c2f74c011ebebfc31
matheusrebola <m.cavalcantir@outlook.com>
2025-02-12 17:15:09 -0300

    feat: cleaning da classe

--- services/persistence/src/main/java/soccervs/persistence/core/services/PassesDetalhesService.java
+++ services/persistence/src/main/java/soccervs/persistence/core/services/PassesDetalhesService.java
@@ -1,19 +1,12 @@
 package soccervs.persistence.core.services;
 
-import java.time.LocalDateTime;
-
 import org.springframework.stereotype.Service;
 
-import soccervs.persistence.core.dtos.Event;
-import soccervs.persistence.core.dtos.History;
 import soccervs.persistence.core.entities.PassesDetalhes;
-import soccervs.persistence.core.enums.ESagaStatus;
 import soccervs.persistence.core.repositories.PassesDetalhesRepository;
 
 @Service
 public class PassesDetalhesService {
-
-	private static final String CURRENT_SOURCE = "INVENTORY_SERVICE";
 
 	private final PassesDetalhesRepository repository;
 
@@ -23,19 +16,6 @@ public class PassesDetalhesService {
 
 	public PassesDetalhes save(PassesDetalhes passesDetalhes) {
 		return repository.save(passesDetalhes);
-	}
-
-	private void handleSuccess(Event event) {
-		event.setStatus(ESagaStatus.SUCCESS);
-		event.setSource(CURRENT_SOURCE);
-		event.getPayload();
-		addHistory(event, "Inventory updated successfully!");
-	}
-
-	private void addHistory(Event event, String message) {
-		var history = History.builder().source(event.getSource()).status(event.getStatus()).message(message)
-				.createdAt(LocalDateTime.now()).build();
-		event.addToHistory(history);
 	}
 
 }
