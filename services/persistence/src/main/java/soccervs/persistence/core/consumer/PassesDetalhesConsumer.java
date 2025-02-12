83e57d806bf9ee353e23b463a290a8230b70d2ac
matheusrebola <m.cavalcantir@outlook.com>
2025-02-12 17:14:07 -0300

    feat: cleaning da classe

--- services/persistence/src/main/java/soccervs/persistence/core/consumer/PassesDetalhesConsumer.java
+++ services/persistence/src/main/java/soccervs/persistence/core/consumer/PassesDetalhesConsumer.java
@@ -1,28 +1,8 @@
 package soccervs.persistence.core.consumer;
 
-import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
-
-import soccervs.persistence.core.services.PassesDetalhesService;
-import soccervs.persistence.core.utils.JsonUtil;
 
 @Component
 public class PassesDetalhesConsumer {
-
-	private final PassesDetalhesService detalhesService;
-	private final JsonUtil jsonUtil;
-
-	public PassesDetalhesConsumer(PassesDetalhesService detalhesService, JsonUtil jsonUtil) {
-		super();
-		this.detalhesService = detalhesService;
-		this.jsonUtil = jsonUtil;
-	}
-
-	@KafkaListener(groupId = "${spring.kafka.consumer.group-id}", topics = "${spring.kafka.topic.persistence-success}")
-	public void consumeSuccessEvent(String payload) {
-		// log.info("Receiving success event {} from inventory-success topic", payload);
-		var event = jsonUtil.toEvent(payload);
-		detalhesService.createPasse(event);
-	}
 
 }
