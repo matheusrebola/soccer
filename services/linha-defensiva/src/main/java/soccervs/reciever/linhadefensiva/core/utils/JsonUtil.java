2ddceea74bef377f19de69aa26db338c4061b8c0
matheusrebola <m.cavalcantir@outlook.com>
2025-02-13 15:45:48 -0300

    feat: configuração do util para transformação de json

---
+++ services/linha-defensiva/src/main/java/soccervs/reciever/linhadefensiva/core/utils/JsonUtil.java
@@ -0,0 +1,33 @@
+package soccervs.reciever.linhadefensiva.core.utils;
+
+import org.springframework.stereotype.Component;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+
+import soccervs.reciever.core.documents.Event;
+
+@Component
+public class JsonUtil {
+	private final ObjectMapper objectMapper;
+	
+    public JsonUtil(ObjectMapper objectMapper) {
+		super();
+		this.objectMapper = objectMapper;
+	}
+
+	public String toJson(Object object) {
+        try {
+            return objectMapper.writeValueAsString(object);
+        } catch (Exception ex) {
+            return "";
+        }
+    }
+
+    public Event toEvent(String json) {
+        try {
+            return objectMapper.readValue(json, Event.class);
+        } catch (Exception ex) {
+            return null;
+        }
+    }
+}
